f6b544bedfe2d78b202ea59190436f4b177ae643
Alexander Gård <alex.gaard@hotmail.com>
2021-10-01 09:51:27 +0200

    Satt opp GH workflows og nais yaml

---
+++ src/test/kotlin/no/nav/amt_arena_ords_proxy/ApplicationTest.kt
@@ -0,0 +1,14 @@
+package no.nav.amt_arena_ords_proxy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("local")
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	fun `application should start without problems`() {}
+
+}
